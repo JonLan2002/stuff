f6e469266b3d7fd380cd31a88acbf6731b6271fe
Jonathan Langela <jonathan.langela@mail.de>
2018-11-10 20:35:29 +0100

    Signed-off-by: Jonathan Langela <jonathan.langela@mail.de>

--- discord/radio/radio.py
+++ discord/radio/radio.py
@@ -11,11 +11,6 @@ client = discord.Client()
 import config
 
 if not discord.opus.is_loaded():
-    # the 'opus' library here is opus.dll on windows
-    # or libopus.so on linux in the current directory
-    # you should replace this with the location the
-    # opus library is located in and with the proper filename.
-    # note that on windows this DLL is automatically provided for you
     discord.opus.load_opus('/usr/local/lib/libopus.so')
 
 @client.event
@@ -25,7 +20,7 @@ async def on_ready():
 	voice.player = voice.create_ffmpeg_player(config.stream)
 	voice.player.start()
 	await client.change_presence(game=discord.Game(name=config.playing, type=0))
-	await client.send_message(discord.User(id=config.owner), '`{0}` started successfully!'.format(client.user))
+	await client.send_message(discord.User(id=config.owner), '{0.name} started successfully!'.format(client.user))
 
 @client.event
 async def on_message(message):
@@ -41,23 +36,21 @@ async def on_message(message):
 		elif message.content.startswith('%stream '):
 			list = message.content.split()
 			try:
-				head = requests.head(list[1])
-				if head.status_code == 200:
-					voice.player.stop()
-					voice.player = voice.create_ffmpeg_player(list[1])
-					voice.player.start()
-					await client.send_message(message.channel, 'The stream has been changed.')
-				else:
-					await client.send_message(message.channel, 'The URL is invalid.')
-			except:
-				await client.send_message(message.channel, 'The URL is invalid.')
+				r = requests.head(list[1], timeout=5)
+				r.raise_for_status()
+				voice.player.stop()
+				voice.player = voice.create_ffmpeg_player(list[1])
+				voice.player.start()
+				await client.send_message(message.channel, 'The stream has been changed.')
+			except requests.RequestException as err:
+				await client.send_message(message.channel, '`{0}`'.format(err))
 		elif message.content.startswith('%channel '):
 			list = message.content.split()
 			channel = client.get_channel(list[1])
 			try:
 				await voice.move_to(channel)
 				await client.send_message(message.channel, 'The channel has been changed.')
-			except:
+			except discord.InvalidArgument:
 				await client.send_message(message.channel, 'Must be a voice channel.')
 		elif message.content.startswith('%playing '):
 			playing = message.content[9:]
@@ -72,7 +65,7 @@ async def on_message(message):
 	if message.channel.is_private:
 		if message.author != client.user:
 			if message.author.id != config.owner:
-				await client.send_message(discord.User(id=config.owner), 'DM from `{0.author}`:\n{0.content}'.format(message))
+				await client.send_message(discord.User(id=config.owner), 'DM from {0.author.mention}:\n{0.content}'.format(message))
 
 @client.event
 async def on_voice_state_update(before, after):
@@ -81,13 +74,13 @@ async def on_voice_state_update(before, after):
 			channel = client.get_channel(config.channel)
 			voice = client.voice_client_in(channel.server)
 			try:
-				if before.voice.voice_channel.id == voice.channel.id:
-					await client.send_message(discord.User(id=config.owner), '`{0}` left the voice channel...'.format(after))
+				if before.voice.voice_channel == voice.channel:
+					await client.send_message(discord.User(id=config.owner), '{0.mention} left the voice channel...'.format(after))
 			except:
 				pass
 			try:
-				if after.voice.voice_channel.id == voice.channel.id:
-					await client.send_message(discord.User(id=config.owner), '`{0}` joined the voice channel!'.format(after))
+				if after.voice.voice_channel == voice.channel:
+					await client.send_message(discord.User(id=config.owner), '{0.mention} joined the voice channel!'.format(after))
 			except:
 				pass
 
